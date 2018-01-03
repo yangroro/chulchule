0d831a5510abb4f6e8e1e0a3ba041bc54e89e9a1
양희찬 <heechan@ridi.com>
2018-01-03 19:16:52 +0900

    asdf

--- chulchule_bot/plugins/late_counter.py
+++ chulchule_bot/plugins/late_counter.py
@@ -1,5 +1,6 @@
 # -*- coding:utf-8 -*-
 import re
+from dateutil.parser import parse
 
 from slackbot import settings
 from slackbot.bot import listen_to
@@ -7,13 +8,23 @@ from slacker import Slacker
 
 
 @listen_to('^이번달 지각리스트', re.IGNORECASE)
-def late_count(message_info:
+def late_count(message_info):
     slacker = Slacker(settings.API_TOKEN)
-
     channel_id = message_info.channel._body['id']
-    history = slacker.channels.history(channel_id, count=1000)
-
-    users = []
-    for message in history:
-
-
+    response = slacker.channels.history(channel_id, count=100)
+    messages = response.body['messages']
+    users = slacker.channels.info(channel_id).body['channel']['members']
+    users = {key: None for key in users}
+    for user in users:
+        users[user] = []
+    for message in messages:
+        if message.get('user'):
+            user = message.get('user')
+            for line in message['text'].split('\n'):
+                try:
+                    late_date = parse(line, fuzzy=True)
+                except ValueError:
+                    continue
+                users[user].append(late_date)
+    print('end')
+    print(users)
